1d8da1ec0741738d70fff6d6d9a90518738518b6
wanghuizhou <wanghuizhou@guazi.com>
2019-02-19 18:16:54 +0800

    textfile - 登录界面

--- routepro/lib/main.dart
+++ routepro/lib/main.dart
@@ -26,11 +26,23 @@ class RouteProSample extends StatefulWidget {
 }
 
 class _RouteProSampleState extends State<RouteProSample> {
+  final TextEditingController _userPhoneController =
+      new TextEditingController();
+  final TextEditingController _userPasswordController =
+      new TextEditingController();
+
+  void onTextClear() {
+    setState(() {
+      _userPhoneController.text = "";
+      _userPasswordController.text = "";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('导航栏标题-button'),
+        title: Text('导航栏标题-登录'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.playlist_play),
@@ -52,17 +64,93 @@ class _RouteProSampleState extends State<RouteProSample> {
         //   ),
         // ),
 
-        child: new RaisedButton(
-          child: new Text(
-            "flutter-button-click",
-            style: new TextStyle(
-              fontSize: 20.0,
-              color: Colors.red,
+        // child: new RaisedButton(
+        //   child: new Text(
+        //     "flutter-button-click",
+        //     style: new TextStyle(
+        //       fontSize: 20.0,
+        //       color: Colors.red,
+        //     ),
+        //   ),
+        //   onPressed: () {
+        //     print("点击了按钮");
+        //   },
+        // ),
+        child: new Column(
+          children: <Widget>[
+            new Text(
+              "文本数据框",
+              style: new TextStyle(
+                fontSize: 20.0,
+              ),
             ),
-          ),
-          onPressed: () {
-            print("点击了按钮");
-          },
+            new TextField(
+              controller: _userPhoneController,
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                contentPadding: const EdgeInsets.all(10.0),
+                icon: new Icon(Icons.phone),
+                labelText: "输入手机号",
+                helperText: "注册时填写的手机号",
+                // suffixIcon: new Icon(Icons.picture_in_picture),
+                // border: new OutlineInputBorder(
+                //   gapPadding: 10.0,
+                //   borderRadius: BorderRadius.circular(20),
+                // ),
+              ),
+              onChanged: (String value) {
+                print("手机号-----》$value]");
+              },
+            ),
+            new TextField(
+              controller: _userPasswordController,
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                contentPadding: const EdgeInsets.all(10.0),
+                icon: new Icon(Icons.nature_people),
+                labelText: "输入姓名",
+                helperText: "注册时填写的姓名",
+                // suffixIcon: new Icon(Icons.picture_in_picture),
+                // border: new OutlineInputBorder(
+                //   gapPadding: 10.0,
+                //   borderRadius: BorderRadius.circular(20),
+                // ),
+              ),
+              onSubmitted: (value) {
+                print("------文字提交------");
+              },
+              onEditingComplete: () {
+                print("------编辑完成------");
+              },
+              onChanged: (value) {
+                print("------输入框中的内容：$value >------");
+              },
+              style: new TextStyle(
+                fontSize: 30.0,
+                color: Colors.red,
+              ),
+            ),
+            new Builder(builder: (BuildContext context) {
+              return new RaisedButton(
+                child: Text(
+                  "debug",
+                  style: new TextStyle(color: Colors.red, fontSize: 20.0),
+                ),
+                highlightColor: Colors.deepPurple,
+                disabledColor: Colors.cyan,
+                onPressed: () {
+                  print("onpredds");
+                  if (_userPhoneController.text.toString() == "10086" &&
+                      _userPasswordController.text.toString() == "10086") {
+                    Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("校验通过")));
+                  } else {
+                    Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("校验有问题")));
+                    onTextClear();
+                  }
+                },
+              );
+            }),
+          ],
         ),
       ),
 
